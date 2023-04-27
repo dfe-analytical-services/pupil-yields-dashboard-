d57c72518a80a21e84d999bc8a949c8f3e0adfc8
Rich Bielby <richard.bielby@education.gov.uk>
2023-04-27 16:19:39 +0100

    Test update

--- tests/shinytest/initial_load_test.R
+++ tests/shinytest/initial_load_test.R
@@ -1,14 +1,34 @@
 app <- ShinyDriver$new("../../", loadTimeout = 6.e4)
 app$snapshotInit("initial_load_test", screenshot = FALSE)
 
-app$snapshot()
+inputs <- c(
+  "filter1",
+  "filter2",
+  "filter3",
+  "filter4",
+  "navlistPanel",
+  "select_breakdown",
+  "select_xaxis",
+  "select_year",
+  "selectArea",
+  "selectLA",
+  "tabsetpanels"
+)
+
+outputs <- c(
+  "bar_headlines",
+  "headline_title",
+  "linePYtime_period"
+)
+
+app$snapshot(list(input = inputs, output = outputs))
 
 app$setInputs(navlistPanel = "dashboard")
-app$snapshot()
+app$snapshot(list(input = inputs, output = outputs))
 
 app$setInputs(navlistPanel = "dashboard", select_xaxis = "School type")
-app$snapshot()
+app$snapshot(list(input = inputs, output = outputs))
 
 
 app$setInputs(navlistPanel = "Technical")
-app$snapshot()
+app$snapshot(list(input = inputs, output = outputs))
