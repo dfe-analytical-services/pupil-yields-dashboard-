82c24e6c74675788f4725ec1e906fc417aa1edb4
WILLIAMS <Sylvia.WILLIAMS@EDUCATION.GOV.UK>
2023-05-04 00:00:15 +0100

    Accessibility statement text

--- R/standard_panels.R
+++ R/standard_panels.R
@@ -6,9 +6,16 @@ a11y_panel <- function() {
         column(
           width = 12,
           h1("Accessibility statement"),
-          br("This accessibility statement applies to the **application name**.
-            This application is run by the Department for Education. We want as many people as possible to be able to use this application,
-            and have actively developed this application with accessibilty in mind."),
+          h2("Using this dashboard"),
+          br("This R-shiny dashboard is run by Department for Education (DfE). We follow the recommendations of the WCAG 2.1 requirements. "),
+          br("We want as many people as possible to be able to use the report by:"),
+          tags$div(tags$ul(
+            tags$li("Navigating most of the dashboard using keyboard and speech recognition software"),
+            tags$li("Use most of the dashboard using a screen reader (including the most recent version of JAWS)"),
+            tags$li("Using assistive technologies such as ZoomText and Fusion"),
+            tags$li("Using different platforms including laptops, tablets and mobile phones"),
+            tags$li("Zoom in up to 300% without the text spilling off the screen")
+            )),
           h2("WCAG 2.1 compliance"),
           br("We follow the reccomendations of the ", a(href = "https://www.w3.org/TR/WCAG21/", "WCAG 2.1 requirements. ", onclick = "ga('send', 'event', 'click', 'link', 'IKnow', 1)"), "This application has been checked using the ", a(href = "https://github.com/ewenme/shinya11y", "Shinya11y tool "), ", which did not detect accessibility issues.
              This application also fully passes the accessibility audits checked by the ", a(href = "https://developers.google.com/web/tools/lighthouse", "Google Developer Lighthouse tool"), ". This means that this application:"),
@@ -19,18 +26,21 @@ a11y_panel <- function() {
           )),
           h2("Limitations"),
           br("We recognise that there are still potential issues with accessibility in this application, but we will continue
-             to review updates to technology available to us to keep improving accessibility for all of our users. For example, these
-            are known issues that we will continue to monitor and improve:"),
-          tags$div(tags$ul(
-            tags$li("List"),
-            tags$li("known"),
-            tags$li("limitations, e.g."),
-            tags$li("Alternative text in interactive charts is limited to titles and could be more descriptive (although this data is available in csv format)")
-          )),
+             to review updates to technology available to us to keep improving accessibility for all of our users."),
+          ##tags$div(tags$ul(
+          ##  tags$li("List"),
+          ##  tags$li("known"),
+          ##  tags$li("limitations, e.g."),
+          ##  tags$li("Alternative text in interactive charts is limited to titles and could be more descriptive (although this data is available in csv format)")
+          ##)),
+          h2("Browser compatibility"),
+          br("The browsers used were Edge Chromium and Chrome as these are standard in the DfE and its agencies. The operating system used was Windows."),
+          br("This statement was prepared on 4th May 2023."),
+          br("This statement was last updated on 4th May 2023."),
           h2("Feedback"),
           br(
-            "If you have any feedback on how we could further improve the accessibility of this application, please contact us at",
-            a(href = "mailto:email@education.gov.uk", "email@education.gov.uk")
+            "If you have any feedback on how we could further improve the accessibility of this application, please contact",
+            a(href = "mailto:sylvia.williams@education.gov.uk", "sylvia.williams@education.gov.uk")
           )
         )
       )
