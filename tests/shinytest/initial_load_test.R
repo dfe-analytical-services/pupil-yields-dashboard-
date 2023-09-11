ce9805c0db10220e18118aa0a63ce456583e269b
Rich Bielby <richard.bielby@education.gov.uk>
2023-09-11 15:53:26 +0100

    Trying sleep command to sort some strange shinytest behaviour

--- tests/shinytest/initial_load_test.R
+++ tests/shinytest/initial_load_test.R
@@ -52,10 +52,12 @@ app$snapshot(list(input = inputs, output = outputs))
 
 message("Set breakdown to housing type")
 app$setInputs(select_breakdown = "Housing type")
+Sys.sleep(2.0)
 app$snapshot(list(input = inputs, output = outputs))
 
 message("Set breakdown to school type")
 app$setInputs(select_breakdown = "School type")
+Sys.sleep(2.0)
 app$snapshot(list(input = inputs, output = outputs))
 
 message("Switch to time series tab")
